8e3488053e0547eadf869c0fa00ea762c2de1da5
Gabriel Gonta <gabriel.gonta@epitech.eu>
2024-09-05 19:00:03 +0200

    Modify App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './Home';
 import Play from './Play';
 import Settings from './Settings';
